dd02c1a75438d3e810104b37c579d49039d70b25
Matt-Bryan <mbryan_379@yahoo.com>
2022-06-15 16:28:47 -0700

    Wrote login logic; Check for existing email on sign up

--- website/auth.py
+++ website/auth.py
@@ -9,6 +9,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                # Login successful
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist', category='error')
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
@@ -23,7 +36,10 @@ def sign_up():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             # Email too short
             flash('Email must be at least 4 characters', category='error')
         elif len(first_name) < 2:
