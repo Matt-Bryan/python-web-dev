742fabbf7ffc404f14f46ff991b9535894c602b5
Matt-Bryan <mbryan_379@yahoo.com>
2022-06-15 17:36:52 -0700

    Creation and Deletion of notes

--- website/views.py
+++ website/views.py
@@ -1,9 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == 'POST':
+        # Attempting to submit a note
+        data = request.form.get('note')
+
+        if len(data) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=data, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note created!', category='success')
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    data = json.loads(request.data)
+    noteId = data['noteId']
+    note = Note.query.get(noteId)
+
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
